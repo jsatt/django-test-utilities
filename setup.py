042e1c25a765f703c0683cb013c0595c5d66229a
Jeremy Satterfield <jsatt22@gmail.com>
2013-02-14 13:48:53 -0600

    add initial version of test_extensions

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 Consumers Unified LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from setuptools import setup
+
+
+setup(
+    name='django-test-extensions',
+    version='0.1',
+    author='Jeremy Satterfield',
+    author_email='jsatterfield@consumeraffairs.com',
+    description='Additional test case methods which are useful within Django',
+    license='Apache License 2.0',
+    url='https://github.com/ConsumerAffairs/django-test-extensions',
+    packages=['test_extensions'],
+    install_requires=['Django>=1.2'],
+    long_description=(
+        'Additional test case methods which are useful within Django'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
---
+++ test_extensions/__init__.py
@@ -0,0 +1,90 @@
+from django.conf import settings
+from django.contrib.messages import DEFAULT_TAGS
+from django.test import TestCase as DjangoTestCase
+from django.utils.importlib import import_module
+
+
+class TestCaseExtensionMixin(object):
+    def assertMessageCount(self, response, expect_num):
+        """
+        Asserts that exactly the given number of messages have been sent.
+        """
+
+        actual_num = len(response.context['messages'])
+        if actual_num != expect_num:
+            self.fail('Message count was %d, expected %d' % (
+                actual_num, expect_num))
+
+    def assertMessageContains(self, response, text, level=None):
+        """
+        Asserts that there is exactly one message containing the given text.
+        """
+
+        messages = response.context['messages']
+
+        matches = [m for m in messages if text in m.message]
+
+        if len(matches) == 1:
+            msg = matches[0]
+            if (level is not None and msg.level != level and
+                    DEFAULT_TAGS[msg.level] != level):
+                msg_level_txt = DEFAULT_TAGS.get(msg.level, 'NO LEVEL')
+                expected_level = DEFAULT_TAGS.get(level, level)
+                self.fail('There was one matching message but with different '
+                          'level: %s != %s' % (msg_level_txt, expected_level))
+            self.assertContains(response, text)
+            return
+
+        elif len(matches) == 0:
+            messages_str = ", ".join('"%s"' % m for m in messages)
+            self.fail('No message contained text "%s", messages were: %s' % (
+                text, messages_str))
+        else:
+            self.fail('Multiple messages contained text "%s": %s' % (
+                text, ", ".join(('"%s"' % m) for m in matches)))
+
+    def assertMessageNotContains(self, response, text):
+        """ Assert that no message contains the given text. """
+
+        messages = response.context['messages']
+
+        matches = [m for m in messages if text in m.message]
+
+        if len(matches) > 0:
+            self.fail('Message(s) should not contain "%s": %s' % (
+                text, ", ".join(('"%s"' % m) for m in matches)))
+
+    def assertNotFormError(self, response, form, field):
+        self.assertEqual(response.context[form][field].errors, [])
+
+    def assertRaisesMessage(self, excClass, callableObj, excMessage,
+                            *args, **kwargs):
+        try:
+            callableObj(*args, **kwargs)
+        except excClass, e:
+            # message_dict and messages for Django's ValidationError
+            if hasattr(e, 'message_dict'):
+                e.message = e.message_dict
+            elif hasattr(e, 'messages'):
+                e.message = e.messages
+
+            if e.message != excMessage:
+                raise AssertionError(
+                    '%s message does not match %r, actual message: %r' %
+                    (excClass.__name__, excMessage, e.message))
+        else:
+            raise AssertionError('%s not raised' % excClass.__name__)
+
+    def get_session(self):
+        engine = import_module(settings.SESSION_ENGINE)
+        if hasattr(self.client.session, 'session_key'):
+            store = engine.SessionStore(self.client.session.session_key)
+        else:
+            store = engine.SessionStore()
+        store.save()  # we need to make load() work, or the cookie is worthless
+        self.client.cookies[settings.SESSION_COOKIE_NAME] = store.session_key
+        return self.client.session
+
+
+class TestCase(TestCaseExtensionMixin, DjangoTestCase):
+    pass
